72e4fa86019a0df367c004846a11dc96d5fa71f4
Basill <91522891+cherkesovbasil@users.noreply.github.com>
2023-11-21 09:28:36 +0300

    Some bugs are fixed, poa_control_unit development has begun

--- first_find_gui.py
+++ first_find_gui.py
@@ -97,6 +97,7 @@ class Gui:
                     if answer:
                         break
 
+
                     com_port_label.config(text="...........")
                     com_port_label.update()
                     time.sleep(0.1)
